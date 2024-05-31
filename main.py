fdf1ed7931e75a67d0c77597a5d83e37ac96f15e
Flaried <80365319+Flaried@users.noreply.github.com>
2024-05-31 05:30:11 -0400

    Updated waits for main

--- main.py
+++ main.py
@@ -4,41 +4,51 @@ import Roblox, time
 print("Made by Flaried")
 
 def main():
-    RobloxAcc = Roblox.RobloxAPIs(Cookie, Proxies=False)
+    RobloxAcc = Roblox.RobloxAPIs(Cookie, Proxies=False) 
     UserdID = RobloxAcc.getUserId()
     Inventory = RobloxAcc.getUserInventory(UserdID)
     with open("Ids.txt", "r") as File:
         data = File.read()
-    for Item in eval(data):
+    for count, Item in enumerate(eval(data)):
         ItemID = Item["id"]
         ItemName = Item["name"]
         productId = Item["productId"]
         SellerID = Item["creatorTargetId"]
         ExpectedPrice = 0
+        count = count +1
+        # We have streak and sleep system to not flood the purchase API (Its a slow API that will error 500)
+        streak = 0
+
         if ItemID not in Inventory:
             retries = 0
-            max_retries = 10
-
+            max_retries = 5
             while retries < max_retries:
                 Response = RobloxAcc.PurchaseItem(int(productId), 1, ExpectedPrice, int(SellerID))
+                streak+=1
+                
                 if Response is not None:
-                    print("[DoggoBuyer] Bought", ItemName)
+                    print(f"[DoggoBuyer ({count} of {len(eval(data))})] Bought", ItemName)
+                    time.sleep(float(Timesleep)*streak)  
+                    if streak >= 25:
+                        streak = 0
+
                     break
                 else:
-                    print("[DoggoBuyer] Couldnt buy", ItemName, "Trying again")
+                    print(f"[DoggoBuyer ({count} of {len(eval(data))})] Couldnt buy", ItemName, "\nTrying again in 55 secs")
                     retries += 1
-                    time.sleep(5)
+                    time.sleep(55)
         else:
-            print("[DoggoBuyer] Already Own", ItemName)
+            print(f"[DoggoBuyer ({count} of {len(eval(data))})] Already Own", ItemName)
 
 if __name__ == "__main__":
     file = 'Settings.cfg'
     config = ConfigParser()
     config.read(file)
     Cookie = config['Authentication']['.ROBLOSECURITY Cookie']
-
+    Timesleep= config['Items']['PurchaseTimeCooldown']
     if config['Items']['RefreshIds'].lower() == 'true':
         Scraper = Roblox.RobloxAPIs(Proxies=True)
         Scraper.scanRobloxCatalog()
 
     main()
+    print("Done buying Items.")
